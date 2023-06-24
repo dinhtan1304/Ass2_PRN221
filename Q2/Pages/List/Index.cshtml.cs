4f7e3247a5338ae8c41aa2f63b3eccccf930ce1b
TanDV <dinhtan.yuki@gmail.com>
2023-06-24 10:52:41 +0700

    Done

--- Q2/Pages/List/Index.cshtml.cs
+++ Q2/Pages/List/Index.cshtml.cs
@@ -20,61 +20,45 @@ namespace Q2.Pages.List
         public string SearchRoom { get; set; }
 
 
-        public async Task OnGetAsync(string searchString, string searchRoom)
+        public async Task OnGetAsync(string searchString)
         {
-            if (_context.Services != null)
-            {
-                Service = await _context.Services
+            Service = await _context.Services
                 .OrderByDescending(item => item.Month)
                 .Include(s => s.EmployeeNavigation)
-                .Include(s => s.RoomTitleNavigation).ToListAsync();
-            }
+                .Include(s => s.RoomTitleNavigation)
+                .ToListAsync();
 
             SearchRoom = searchString;
             IQueryable<Service> servicesSearch = from s in _context.Services select s;
             if (!String.IsNullOrEmpty(searchString))
             {
                 servicesSearch = servicesSearch.Where(s => s.RoomTitle.Contains(searchString));
-                Service = servicesSearch.ToList();
+                Service = await servicesSearch.ToListAsync();
             }
 
         }
-        public ActionResult ImportServices()
-        {
-            return Page();
-        }
 
-        [HttpPost]
-        public ActionResult ImportServices(IFormFile file)
+        public async Task<IActionResult> OnPostImportServices(IFormFile file)
         {
             if (file != null && file.Length > 0)
             {
-                // Đọc tệp XML được tải lên
-                var serializer = new XmlSerializer(typeof(ServiceXML));
-                ServiceXML services;
-
-                using (var reader = new StreamReader(file.OpenReadStream()))
+                using (var stream = new MemoryStream())
                 {
-                    services = (ServiceXML)serializer.Deserialize(reader);
+                    await file.CopyToAsync(stream);
+                    stream.Position = 0;
+
+                    var serializer = new XmlSerializer(typeof(List<Service>));
+                    var services = serializer.Deserialize(stream) as List<Service>;
+                    await _context.Services.AddRangeAsync(services);
+                    await _context.SaveChangesAsync();
                 }
-
-                // Lưu danh sách dịch vụ vào cơ sở dữ liệu
-                using (var context = new Prn221TrialContext())
-                {
-                    foreach (var service in services.Services)
-                    {
-                        context.Services.Add(service);
-                    }
-
-                    context.SaveChanges();
-                }
-
-                // Chuyển hướng người dùng đến trang hiển thị danh sách dịch vụ
-                return RedirectToAction("List");
             }
-
-            // Trường hợp không có tệp được chọn, trả về lại View
-            return Page();
+            Service = await _context.Services
+                        .OrderByDescending(item => item.Month)
+                        .Include(s => s.EmployeeNavigation)
+                        .Include(s => s.RoomTitleNavigation)
+                        .ToListAsync();
+            return RedirectToPage();
         }
     }
 }
